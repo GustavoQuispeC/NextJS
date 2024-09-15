62ef8ccb1937d424cf78390261f7d3470b17d36e
Gustavo <gusstavocta@gmail.com>
2024-09-14 23:20:22 -0500

    modificando comentario

--- 02-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,33 +1,31 @@
 import { PokemonsResponse, PokemosnGrid, SimplePokemon } from "@/app/pokemons";
 
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((response) => response.json());
 
-
-
-const getPokemons = async (limit=20, offset=0):Promise<SimplePokemon[]> => {
-  const data : PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
-  .then(response => response.json());
-
-  const pokemons = data.results.map(pokemon => ({
-      id: pokemon.url.split('/').at(-2) as string,
-      name: pokemon.name,
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2) as string,
+    name: pokemon.name,
   }));
 
   return pokemons;
-}
+};
 
 export default async function PokemonsPage() {
-    const pokemons = await getPokemons(200);
-   
+  const pokemons = await getPokemons(200);
+
   return (
     <div className="flex flex-col">
-        <span className="text-5xl my-2">Listado de Pokemons <small> Estaticos</small> </span>
-       
-        <PokemosnGrid 
-        pokemons={pokemons} />
+      <span className="text-5xl my-2">
+        Listado de Pokemones <small> Estaticos</small>{" "}
+      </span>
 
-       
-       
+      <PokemosnGrid pokemons={pokemons} />
     </div>
   );
 }
-
--- 02-dashboard/src/app/pokemons/components/PokemonCard.tsx
+++ 02-dashboard/src/app/pokemons/components/PokemonCard.tsx
@@ -12,53 +12,53 @@ export const PokemonCard = ({ pokemon }: Props) => {
 
   return (
     <div className="mx-auto mt-4 w-52">
-    {/* Establecer un alto mínimo para asegurar el tamaño uniforme */}
-    <div className="bg-white rounded-lg overflow-hidden shadow-lg flex flex-col justify-between">
-      {/* Sección superior con la imagen */}
-      <div className="h-60 bg-gray-800 border-b flex flex-col justify-between">
-        {/* Centrando la imagen */}
-        <div className="flex justify-center pt-5">
-          <Image
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-            width={100}
-            height={100}
-            alt={name}
-          />
+      {/* Establecer un alto mínimo para asegurar el tamaño uniforme */}
+      <div className="bg-white rounded-lg overflow-hidden shadow-lg flex flex-col justify-between">
+        {/* Sección superior con la imagen */}
+        <div className="h-60 bg-gray-800 border-b flex flex-col justify-between">
+          {/* Centrando la imagen */}
+          <div className="flex justify-center pt-5">
+            <Image
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+              width={100}
+              height={100}
+              alt={name}
+            />
+          </div>
+
+          {/* Contenedor inferior: nombre y botón */}
+          <div className="px-4 pb-4 flex flex-col justify-end items-center">
+            {/* Nombre del Pokémon */}
+            <p className="text-lg font-semibold text-gray-50">{name}</p>
+
+            {/* Botón más información */}
+            <Link
+              href={`dashboard/pokemon/${id}`}
+              className="mt-2 border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
+            >
+              Más información
+            </Link>
+          </div>
         </div>
 
-        {/* Contenedor inferior: nombre y botón */}
-        <div className="px-4 pb-4 flex flex-col justify-end items-center">
-          {/* Nombre del Pokémon */}
-          <p className="text-lg font-semibold text-gray-50">{name}</p>
-
-          {/* Botón más información */}
+        {/* Sección de enlaces con espaciado */}
+        <div className="border-b">
           <Link
-            href={`dashboard/pokemon/${id}`}
-            className="mt-2 border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
+            href="/dashboard/main"
+            className="px-4 py-2 hover:bg-gray-100 flex items-center"
           >
-            Más información
+            <div className="text-red-600">
+              <IoHeartOutline className="w-5 h-5" />
+            </div>
+            <div className="pl-3">
+              <p className="text-sm font-medium text-gray-800 leading-none">
+                No es Favorito
+              </p>
+              <p className="text-xs text-gray-500">View your campaigns</p>
+            </div>
           </Link>
         </div>
       </div>
-
-      {/* Sección de enlaces con espaciado */}
-      <div className="border-b">
-        <Link
-          href="/dashboard/main"
-          className="px-4 py-2 hover:bg-gray-100 flex items-center"
-        >
-          <div className="text-red-600">
-            <IoHeartOutline className="w-5 h-5" />
-          </div>
-          <div className="pl-3">
-            <p className="text-sm font-medium text-gray-800 leading-none">
-              No es Favorito
-            </p>
-            <p className="text-xs text-gray-500">View your campaigns</p>
-          </div>
-        </Link>
-      </div>
     </div>
-  </div>
   );
 };
