a67fecc8d99d5da60d3a1222ddb9b90da607a59e
Gustavo <gusstavocta@gmail.com>
2024-09-15 23:33:45 -0500

    finalizando temario

--- 02-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,33 +1,35 @@
-import { PokemonsResponse, PokemonsGrid, SimplePokemon } from "@/pokemons";
+import { PokemonGrid, PokemonsReponse, SimplePokemon } from "@/pokemons";
 
-const getPokemons = async (
-  limit = 20,
-  offset = 0
-): Promise<SimplePokemon[]> => {
-  const data: PokemonsResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
-  ).then((response) => response.json());
 
-  const pokemons = data.results.map((pokemon) => ({
-    id: pokemon.url.split("/").at(-2) as string,
-    name: pokemon.name,
-  }));
-  //throw new ('esto es un error de prueba');
-  return pokemons;
-};
+const getPokemons = async( limit = 20, offset= 0 ):Promise<SimplePokemon[]> => {
+  const data:PokemonsReponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${ offset }`)
+    .then( res => res.json() );
+
+    const pokemons = data.results.map( pokemon => ({
+      id: pokemon.url.split('/').at(-2)!,
+      name: pokemon.name,
+    }));
+
+    // throw new Error('Esto es un error que no debería de suceder');
+    // throw notFound();
+
+    return pokemons;
+}
+
+
+
 
 export default async function PokemonsPage() {
-  const pokemons = await getPokemons(200);
 
+  const pokemons = await getPokemons(151);
+  
   return (
-    <>
-      <div className="flex flex-col">
-        <span className="text-5xl my-2">
-          Listado de Pokemones <small> Estaticos</small>{" "}
-        </span>
+    <div className="flex flex-col">
 
-        <PokemonsGrid pokemons={pokemons} />
-      </div>
-    </>
+      <span className="text-5xl my-2">Listado de Pokémons <small>estático</small></span>
+      
+      <PokemonGrid pokemons={ pokemons } />
+
+    </div>
   );
 }
